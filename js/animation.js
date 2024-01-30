fe569bc92cae3d9384ce31dfa398e6200f852c8a
Tismaa78 <mathisroussel78@gmail.com>
2024-01-30 16:17:58 +0100

    Premier commit

---
+++ js/animation.js
@@ -0,0 +1,3 @@
+window.sr = ScrollReveal();
+sr.reveal('.row' , { delay: 200, easing: 'ease', duration: 400, scale: 0, distance: '80px', mobile: false });
+sr.reveal('.carousel-caption' , { delay: 200, easing: 'ease', duration: 400, scale: 0, distance: '0px', mobile: false });
